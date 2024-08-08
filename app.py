8707e6991f66e60ba84c670ecbb1b10cff49331e
KHW <127100063+khw-dev@users.noreply.github.com>
2024-08-08 21:23:35 +0900

    feat: Create day2 project

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,32 +8,8 @@ post_data = [
         "author": "asdf",
         "date": "2024-08-06",
         "content": "오늘은 Flask로 블로그를 만들어보았다.",
-    },{
-        "title": "홈페이지 만들기",
-        "author": "asdf",
-        "date": "2024-08-07",
-        "content": "오늘은 Flask로 홈페이지를 만들어보았다.",
-    },{
-        "title": "db 연동하기",
-        "author": "asdf",
-        "date": "2024-08-08",
-        "content": "오늘은 sqlite를 연동해보았다.",
     }
 ]
-
-def init_db():
-    conn = sqlite3.connect("feedback.db")
-    c = conn.cursor()
-    c.execute(
-        """
-        CREATE TABLE IF NOT EXISTS feedbacks
-        (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, message TEXT)
-        """
-    )
-    conn.commit()
-    conn.close()
-
-init_db()
 
 @app.route("/")
 def index():
@@ -45,29 +20,6 @@ def index():
 def post(post_id):
     post = post_data[post_id - 1]
     return render_template("post.html", post=post)
-
-@app.route("/feedback", methods=["GET", "POST"])
-def feedback():
-    if request.method == "POST":
-        name = request.form["name"]
-        message = request.form["message"]
-        conn = sqlite3.connect("feedback.db")
-        c = conn.cursor()
-        c.execute("INSERT INTO feedbacks (name, message) VALUES (?, ?)", (name, message))
-        conn.commit()
-        conn.close()
-        return redirect(url_for("show_feedback"))
-    return render_template("feedback.html")
-
-@app.route("/show_feedback")
-def show_feedback():
-    conn = sqlite3.connect("feedback.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM feedbacks")
-    feedbacks = c.fetchall()
-    print(feedbacks)
-    conn.close()
-    return render_template("show_feedback.html", feedbacks=feedbacks)
 
 
 if __name__ == "__main__":
